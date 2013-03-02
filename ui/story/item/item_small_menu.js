45ee65eda41db77d6408f83247ae61169908e3ae
Raphael Korach <raphael.korach@gmail.com>
2013-03-02 00:50:31 -0300

    Refactor: bad indent

--- ui/story/item/item_small_menu.js
+++ ui/story/item/item_small_menu.js
@@ -10,10 +10,10 @@ Cotton.UI.Story.Item.SmallMenu = Class
 
       _$itemMenu : null,
 
-			_$remove : null,
-  		_$openLink : null,
-  		_$open : null,
-	  	_$expand : null,
+      _$remove : null,
+      _$openLink : null,
+      _$open : null,
+      _$expand : null,
 
       init : function(oItemContent) {
         var self = this;
@@ -21,22 +21,22 @@ Cotton.UI.Story.Item.SmallMenu = Class
         // current parent element.
         this._oItemContent = oItemContent;
 
-       // current item
+        // current item
         this._$itemMenu = $('<div class="ct-label-small-menu"></div>');
-				
-        // current sub elements
-		    this._$remove = $('<p>Remove</p>');
-		    this._$openLink = $('<a href="" target="blank"></a>');
-		    this._$open = $('<p>Open</p>');
-		    this._$expand = $('<p>Expand</p>');
 
-				// url
-		    var sUrl = this._oItemContent.item().visitItem().url();
-		    self._$openLink.attr('href',sUrl);
-		
+        // current sub elements
+        this._$remove = $('<p>Remove</p>');
+        this._$openLink = $('<a href="" target="blank"></a>');
+        this._$open = $('<p>Open</p>');
+        this._$expand = $('<p>Expand</p>');
+
+        // url
+        var sUrl = this._oItemContent.item().visitItem().url();
+        self._$openLink.attr('href',sUrl);
+
         // construct item
         self._$itemMenu.append(
-		  		  self._$openLink.append(self._$open)
+        self._$openLink.append(self._$open)
         );
       },
 
