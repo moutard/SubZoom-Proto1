d509614aeffa6b2fb634db3351d76fbed8c868ec
Raphael Korach <raphael.korach@gmail.com>
2013-03-03 00:17:39 -0300

    Refactor: bad indent

--- ui/story/item/item_website.js
+++ ui/story/item/item_website.js
@@ -8,9 +8,9 @@ Cotton.UI.Story.Item.Website = Class
 
       _oItemContent : null,
 
-			_$itemWebsite : null,
-			_$favicon : null,
-			_$url : null,
+      _$itemWebsite : null,
+      _$favicon : null,
+      _$url : null,
 
       init : function(oItemContent) {
         var self = this;
@@ -18,30 +18,30 @@ Cotton.UI.Story.Item.Website = Class
         // current parent element.
         this._oItemContent = oItemContent;
 				
-				this._$itemWebsite = $('<div class="website"></div>'); 
-				this._$favicon = $('<img class="favicon">');
-				this._$url = $('<div class="url"></div>');
-				
+        this._$itemWebsite = $('<div class="website"></div>'); 
+        this._$favicon = $('<img class="favicon">');
+        this._$url = $('<div class="url"></div>');
+		  	
         // set values
-
+        
         // url
-				var sUrl = this._oItemContent.item().visitItem().url();
-				// Extracts www.google.fr from http://www.google.fr/abc/def?q=deiubfds.
-				var oReg = new RegExp("\/\/([^/]*)\/");
-				var sDomain = sUrl.match(oReg)[1];
-				this._$url.text(sDomain);
-
-				//favicon
-		    var sFavicon = this._oItemContent.item().visitItem().favicon();
-				  if (sFavicon === "") {
-					  this._$favicon = $('<img class="favicon" src="chrome://favicon/http://'+sDomain+'/">');
-					}
+        var sUrl = this._oItemContent.item().visitItem().url();
+        // Extracts www.google.fr from http://www.google.fr/abc/def?q=deiubfds.
+        var oReg = new RegExp("\/\/([^/]*)\/");
+        var sDomain = sUrl.match(oReg)[1];
+        this._$url.text(sDomain);
+        
+        //favicon
+        var sFavicon = this._oItemContent.item().visitItem().favicon();
+        if (sFavicon === "") {
+          this._$favicon = $('<img class="favicon" src="chrome://favicon/http://'+sDomain+'/">');
+        }
 		
         // construct item
-	  		self._$itemWebsite.append(
-		  		self._$favicon,
-			  	self._$url
-				);
+        self._$itemWebsite.append(
+          self._$favicon,
+          self._$url
+        );
 
       },
 
