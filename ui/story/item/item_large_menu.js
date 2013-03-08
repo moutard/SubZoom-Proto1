8a09867899fff8d63071c52b43b6f0a51d5e8dcd
Raphael Korach <raphael.korach@gmail.com>
2013-03-08 20:40:32 -0300

    UI: add 'remove' button on large labels

--- ui/story/item/item_large_menu.js
+++ ui/story/item/item_large_menu.js
@@ -10,9 +10,9 @@ Cotton.UI.Story.Item.LargeMenu = Class
 
       _$itemLargeMenu : null,
 
-			_$remove : null,
-  		_$openLink : null,
-  		_$open : null,
+      _$remove : null,
+      _$openLink : null,
+      _$open : null,
 
       init : function(oItemContent) {
         var self = this;
@@ -20,22 +20,36 @@ Cotton.UI.Story.Item.LargeMenu = Class
         // current parent element.
         this._oItemContent = oItemContent;
 
-       // current item
+        // current item
         this._$itemLargeMenu = $('<div class="ct-label-large-menu"></div>');
-				
+
         // current sub elements
-		    this._$remove = $('<p>Remove</p>');
-		    this._$openLink = $('<a href="" target="blank"></a>');
-		    this._$open = $('<p>Open</p>');
-		
+        this._$remove = $('<p>Remove</p>');
+        this._$openLink = $('<a href="" target="blank"></a>');
+        this._$open = $('<p>Open</p>');
+
         // set values
-				// url
-		    var sUrl = this._oItemContent.item().visitItem().url();
-		    self._$openLink.attr('href',sUrl);
-		
+        // url
+        var sUrl = this._oItemContent.item().visitItem().url();
+        self._$openLink.attr('href',sUrl);
+
+        //remove element
+        this._$remove.click(function(){
+          //TODO(rkorach): use only one db for the whole UI
+          self._oDatabase = new Cotton.DB.IndexedDB.Wrapper('ct', {
+              'visitItems' : Cotton.Translators.VISIT_ITEM_TRANSLATORS
+          }, function() {
+            self._oDatabase.delete('visitItems', self._oItemContent.item().visitItem().id(),
+              function() {
+        	    self._oItemContent.item().container().isotope('remove', self._oItemContent.item().$());
+            });
+          });
+        });
+
         // construct item
         self._$itemLargeMenu.append(
-		  		  self._$openLink.append(self._$open)
+          self._$openLink.append(self._$open),
+          self._$remove
         );
       },
 
